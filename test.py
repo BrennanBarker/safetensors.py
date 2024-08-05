8f40e704530535f644970eadf464e38363db7c49
Brennan Barker <19918186+BrennanBarker@users.noreply.github.com>
2024-08-05 00:56:41 +0000

    rename

--- test.py
+++ test.py
@@ -3,18 +3,18 @@ from tqdm import tqdm
 
 from archive import get_header
 
-np = 'regenerated.safetensors'
-np_header = get_header(np)
-np_metadata = np_header.pop('__metadata__')
+re = 'regenerated.safetensors'
+re_header = get_header(re)
+re_metadata = re_header.pop('__metadata__')
 
 tr ='sd3_medium.safetensors'
 tr_header = get_header(tr)
 tr_metadata = tr_header.pop('__metadata__')
 
-assert np_header == tr_header
-assert np_metadata == tr_metadata
+assert re_header == tr_header
+assert re_metadata == tr_metadata
 
-np_tensors = safetensors.safe_open(np, framework='numpy')
+re_tensors = safetensors.safe_open(re, framework='numpy')
 tr_tensors = safetensors.safe_open(tr, framework='numpy')
 for key in tqdm(list(tr_header.keys())):
-    assert (np_tensors.get_tensor(key) == tr_tensors.get_tensor(key)).all()  # type: ignore
+    assert (re_tensors.get_tensor(key) == tr_tensors.get_tensor(key)).all()  # type: ignore
