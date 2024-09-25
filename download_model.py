58eb93266fe2189553c9b4923ce61f0beadd5679
Brennan Barker <19918186+BrennanBarker@users.noreply.github.com>
2024-09-25 01:34:07 +0000

    rm download model

--- download_model.py
+++
@@ -1,5 +0,0 @@
-import huggingface_hub
-import os
-
-file_path = huggingface_hub.hf_hub_download('stabilityai/stable-diffusion-3-medium', filename='text_encoders/clip_l.safetensors', token=os.environ['HF_ACCESS_KEY'])
-print(file_path)
